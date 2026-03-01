e4d53b0a8312d2e7bc70d6178a38778b5d1beff5
DamAb-CSh2026 <fortrash1919@gmail.com>
2026-03-01 17:01:17 +0500

    Add files via upload

---
+++ project6/project6/dz61.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace StrategyPatternDemo
+{
+    public interface IPaymentStrategy
+    {
+        void Pay(decimal amount);
+    }
+
+
+    public class CreditCardPayment : IPaymentStrategy
+    {
+        private string cardNumber;
+
+        public CreditCardPayment(string cardNumber)
+        {
+            this.cardNumber = cardNumber;
+        }
+
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Оплата {amount} тг банковской картой {cardNumber}");
+        }
+    }
+
+    public class PayPalPayment : IPaymentStrategy
+    {
+        private string email;
+
+        public PayPalPayment(string email)
+        {
+            this.email = email;
+        }
+
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Оплата {amount} тг через PayPal аккаунт {email}");
+        }
+    }
+
+    public class CryptoPayment : IPaymentStrategy
+    {
+        private string walletAddress;
+
+        public CryptoPayment(string walletAddress)
+        {
+            this.walletAddress = walletAddress;
+        }
+
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Оплата {amount} тг криптовалютой с кошелька {walletAddress}");
+        }
+    }
+
+    public class PaymentContext
+    {
+        private IPaymentStrategy _paymentStrategy;
+
+        public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void ExecutePayment(decimal amount)
+        {
+            if (_paymentStrategy == null)
+            {
+                Console.WriteLine("Способ оплаты не выбран!");
+                return;
+            }
+
+            _paymentStrategy.Pay(amount);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentContext context = new PaymentContext();
+
+            Console.WriteLine("Выберите способ оплаты:");
+            Console.WriteLine("1 - Банковская карта");
+            Console.WriteLine("2 - PayPal");
+            Console.WriteLine("3 - Криптовалюта");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    context.SetPaymentStrategy(new CreditCardPayment("1234-5678-9999-0000"));
+                    break;
+
+                case "2":
+                    context.SetPaymentStrategy(new PayPalPayment("user@mail.com"));
+                    break;
+
+                case "3":
+                    context.SetPaymentStrategy(new CryptoPayment("0xA45F9B88C"));
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный выбор");
+                    return;
+            }
+
+            context.ExecutePayment(5000);
+        }
+    }
+}
---
+++ project6/project6/dz62.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPatternDemo
+{
+    public interface IObserver
+    {
+        void Update(string currency, decimal rate);
+    }
+
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify(string currency, decimal rate);
+    }
+
+    public class CurrencyExchange : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        private Dictionary<string, decimal> _rates = new Dictionary<string, decimal>();
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void SetRate(string currency, decimal rate)
+        {
+            _rates[currency] = rate;
+            Console.WriteLine($"\nКурс {currency} обновлён: {rate}");
+            Notify(currency, rate);
+        }
+
+        public void Notify(string currency, decimal rate)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(currency, rate);
+            }
+        }
+    }
+
+
+    public class BankObserver : IObserver
+    {
+        public void Update(string currency, decimal rate)
+        {
+            Console.WriteLine($"[Банк] Новый курс {currency}: {rate}");
+        }
+    }
+
+    public class InvestorObserver : IObserver
+    {
+        public void Update(string currency, decimal rate)
+        {
+            if (rate > 500)
+                Console.WriteLine($"[Инвестор] Курс высокий! Возможно продавать {currency}");
+            else
+                Console.WriteLine($"[Инвестор] Курс низкий. Можно покупать {currency}");
+        }
+    }
+
+    public class MobileAppObserver : IObserver
+    {
+        public void Update(string currency, decimal rate)
+        {
+            Console.WriteLine($"[Мобильное приложение] Push-уведомление: {currency} = {rate}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurrencyExchange exchange = new CurrencyExchange();
+
+            IObserver bank = new BankObserver();
+            IObserver investor = new InvestorObserver();
+            IObserver app = new MobileAppObserver();
+
+            exchange.Attach(bank);
+            exchange.Attach(investor);
+            exchange.Attach(app);
+
+            exchange.SetRate("USD", 495);
+            exchange.SetRate("USD", 510);
+
+            exchange.Detach(bank);
+
+            exchange.SetRate("EUR", 530);
+
+            Console.ReadLine();
+        }
+    }
+}
