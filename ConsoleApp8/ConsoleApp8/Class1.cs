140cb4ad62167d4ac5b21c1781d16f93e051e02e
DamAb-CSh2026 <fortrash1919@gmail.com>
2026-03-01 17:00:20 +0500

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public interface IObserver
+    {
+        void Update(double rate);
+    }
+
+    public interface ISubject
+    {
+        void AddObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void Notify();
+    }
+
+    public class CurrencyExchange : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        private double rate;
+
+        public void SetRate(double newRate)
+        {
+            rate = newRate;
+            Notify();
+        }
+
+        public void AddObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in observers)
+                observer.Update(rate);
+        }
+    }
+    public class Bank : IObserver
+    {
+        public void Update(double rate)
+        {
+            Console.WriteLine($"Банк получил новый курс: {rate}");
+        }
+    }
+    public class Investor : IObserver
+    {
+        public void Update(double rate)
+        {
+            Console.WriteLine($"Инвестор анализирует курс: {rate}");
+        }
+    }
+    public class MobileApp : IObserver
+    {
+        public void Update(double rate)
+        {
+            Console.WriteLine($"Мобильное приложение уведомляет пользователя: {rate}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            CurrencyExchange exchange = new CurrencyExchange();
+
+            IObserver bank = new Bank();
+            IObserver investor = new Investor();
+            IObserver app = new MobileApp();
+
+            exchange.AddObserver(bank);
+            exchange.AddObserver(investor);
+            exchange.AddObserver(app);
+
+            Console.WriteLine("Изменение курса...");
+            exchange.SetRate(470.5);
+
+            Console.WriteLine("\nУдаляем инвестора...\n");
+            exchange.RemoveObserver(investor);
+
+            exchange.SetRate(480.0);
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public interface IPaymentStrategy
+{
+    void Pay(double amount);
+}
+public class CardPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} тенге банковской картой");
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} тенге через PayPal");
+    }
+}
+
+public class CryptoPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} тенге криптовалютой");
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy strategy;
+    public void SetStrategy(IPaymentStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+    public void Pay(double amount)
+    {
+        strategy.Pay(amount);
+    }
+}
+
+class Program
+{
+    staticvoid Main()
+    {
+        PaymentContext context = new PaymentContext();
+
+        Console.WriteLine("Выберите способ оплаты: 1 - Карта, 2 - PayPal, 3 - Крипта");
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 1)
+            context.SetStrategy(new CardPayment());
+        else if (choice == 2)
+            context.SetStrategy(new PayPalPayment());
+        else
+            context.SetStrategy(new CryptoPayment());
+
+        context.Pay(1000);
+    }
+}
