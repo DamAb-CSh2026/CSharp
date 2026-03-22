88e24a499936df590aa869129c2a8c53e59ef0d9
DamAb-CSh2026 <fortrash1919@gmail.com>
2026-03-22 19:20:10 +0500

    Add files via upload

---
+++ T8/T8/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8
+{
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(double amount);
+    }
+
+    public class PayPalPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Оплата {amount} через PayPal выполнена");
+        }
+    }
+
+    public class StripePaymentService
+    {
+        public void MakeTransaction(double totalAmount)
+        {
+            Console.WriteLine($"Stripe провёл транзакцию на {totalAmount}");
+        }
+    }
+
+    public class StripePaymentAdapter : IPaymentProcessor
+    {
+        private StripePaymentService stripe = new StripePaymentService();
+
+        public void ProcessPayment(double amount)
+        {
+            stripe.MakeTransaction(amount);
+        }
+    }
+
+    public class CryptoPaymentService
+    {
+        public void SendCrypto(double sum)
+        {
+            Console.WriteLine($"Крипто-платёж на {sum} отправлен");
+        }
+    }
+
+    public class CryptoPaymentAdapter : IPaymentProcessor
+    {
+        private CryptoPaymentService crypto = new CryptoPaymentService();
+
+        public void ProcessPayment(double amount)
+        {
+            crypto.SendCrypto(amount);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            IPaymentProcessor paypal = new PayPalPaymentProcessor();
+            IPaymentProcessor stripe = new StripePaymentAdapter();
+            IPaymentProcessor crypto = new CryptoPaymentAdapter();
+
+            paypal.ProcessPayment(1000);
+            stripe.ProcessPayment(2000);
+            crypto.ProcessPayment(3000);
+        }
+    }
+}
---
+++ T8/T8/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+public abstract class Beverage
+{
+    public abstract string GetDescription();
+    public abstract int Cost();
+}
+
+public class Espresso : Beverage
+{
+    public override string GetDescription() => "Эспрессо";
+    public override int Cost() => 800;
+}
+
+public class Tea : Beverage
+{
+    public override string GetDescription() => "Чай";
+    public override int Cost() => 500;
+}
+
+public class Latte : Beverage
+{
+    public override string GetDescription() => "Латте";
+    public override int Cost() => 1000;
+}
+
+public class Mocha : Beverage
+{
+    public override string GetDescription() => "Мокко";
+    public override int Cost() => 1200;
+}
+
+public abstract class Addon : Beverage
+{
+    protected Beverage drink;
+
+    public Addon(Beverage drink)
+    {
+        this.drink = drink;
+    }
+}
+
+public class Milk : Addon
+{
+    public Milk(Beverage d) : base(d) { }
+
+    public override string GetDescription() => drink.GetDescription() + ", молоко";
+    public override int Cost() => drink.Cost() + 200;
+}
+
+public class Sugar : Addon
+{
+    public Sugar(Beverage d) : base(d) { }
+
+    public override string GetDescription() => drink.GetDescription() + ", сахар";
+    public override int Cost() => drink.Cost() + 50;
+}
+
+public class Cream : Addon
+{
+    public Cream(Beverage d) : base(d) { }
+
+    public override string GetDescription() => drink.GetDescription() + ", сливки";
+    public override int Cost() => drink.Cost() + 300;
+}
+
+public class Caramel : Addon
+{
+    public Caramel(Beverage d) : base(d) { }
+
+    public override string GetDescription() => drink.GetDescription() + ", карамель";
+    public override int Cost() => drink.Cost() + 250;
+}
+
+public class Chocolate : Addon
+{
+    public Chocolate(Beverage d) : base(d) { }
+
+    public override string GetDescription() => drink.GetDescription() + ", шоколад";
+    public override int Cost() => drink.Cost() + 300;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Beverage b1 = new Espresso();
+        b1 = new Milk(b1);
+        b1 = new Sugar(b1);
+
+        Console.WriteLine(b1.GetDescription());
+        Console.WriteLine("Цена: " + b1.Cost());
+
+        Console.WriteLine();
+
+        Beverage b2 = new Latte();
+        b2 = new Caramel(b2);
+        b2 = new Cream(b2);
+
+        Console.WriteLine(b2.GetDescription());
+        Console.WriteLine("Цена: " + b2.Cost());
+
+        Console.WriteLine();
+
+        Beverage b3 = new Mocha();
+        b3 = new Chocolate(b3);
+        b3 = new Milk(b3);
+        b3 = new Sugar(b3);
+
+        Console.WriteLine(b3.GetDescription());
+        Console.WriteLine("Цена: " + b3.Cost());
+    }
+}
