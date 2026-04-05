cdea4adbe967b132da5c45b8a03323fda0d72787
DamAb-CSh2026 <fortrash1919@gmail.com>
2026-04-05 17:40:02 +0500

    Add files via upload

---
+++ T9/T9/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+class TV
+{
+    public void On() => Console.WriteLine("TV on");
+    public void Off() => Console.WriteLine("TV off");
+    public void SetChannel(string channel) => Console.WriteLine($"TV channel set: {channel}");
+    public void SetInput(string input) => Console.WriteLine($"TV input set: {input}");
+}
+class AudioSystem
+{
+    private int volume = 10;
+
+    public void On() => Console.WriteLine("Audiosystem on");
+    public void Off() => Console.WriteLine("Audiosystem off");
+
+    public void SetVolume(int level)
+    {
+        volume = level;
+        Console.WriteLine($"Volume set on: {volume}");
+    }
+}
+class DVDPlayer
+{
+    public void On() => Console.WriteLine("DVD On");
+    public void Off() => Console.WriteLine("DVD off");
+
+    public void Play() => Console.WriteLine("Play DVD");
+    public void Pause() => Console.WriteLine("Pause DVD");
+    public void Stop() => Console.WriteLine("Stop DVD");
+}
+class GameConsole
+{
+    public void On() => Console.WriteLine("Игровая консоль включена");
+    public void Off() => Console.WriteLine("Игровая консоль выключена");
+
+    public void StartGame() => Console.WriteLine("Игра запущена");
+}
+class HomeTheaterFacade
+{
+    private TV tv;
+    private AudioSystem audio;
+    private DVDPlayer dvd;
+    private GameConsole game;
+
+    public HomeTheaterFacade(TV tv, AudioSystem audio, DVDPlayer dvd, GameConsole game)
+    {
+        this.tv = tv;
+        this.audio = audio;
+        this.dvd = dvd;
+        this.game = game;
+    }
+    public void WatchMovie()
+    {
+        Console.WriteLine("\n Mode:Movie On");
+        tv.On();
+        tv.SetInput("DVD");
+        audio.On();
+        audio.SetVolume(15);
+        dvd.On();
+        dvd.Play();
+    }
+    public void StopMovie()
+    {
+        Console.WriteLine("\n Movie Stop");
+        dvd.Stop();
+        dvd.Off();
+        audio.Off();
+        tv.Off();
+    }
+    public void PlayGame()
+    {
+        Console.WriteLine("\n Mode:Game");
+        tv.On();
+        tv.SetInput("Game");
+        game.On();
+        game.StartGame();
+        audio.On();
+        audio.SetVolume(20);
+    }
+    public void ListenMusic()
+    {
+        Console.WriteLine("\n Mode:Music");
+        tv.On();
+        tv.SetInput("Audio");
+        audio.On();
+        audio.SetVolume(25);
+    }
+    public void AllOff()
+    {
+        Console.WriteLine("\n All system's off");
+        dvd.Off();
+        game.Off();
+        audio.Off();
+        tv.Off();
+    }
+    public void SetVolume(int level)
+    {
+        audio.SetVolume(level);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        TV tv = new TV();
+        AudioSystem audio = new AudioSystem();
+        DVDPlayer dvd = new DVDPlayer();
+        GameConsole game = new GameConsole();
+
+        HomeTheaterFacade home = new HomeTheaterFacade(tv, audio, dvd, game);
+
+        home.WatchMovie();
+        home.SetVolume(18);
+        home.StopMovie();
+
+        home.PlayGame();
+        home.AllOff();
+
+        home.ListenMusic();
+        home.AllOff();
+    }
+}
---
+++ T9/T9/Task2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9
+{
+    abstract class FileSystemComponent
+    {
+        public string Name;
+
+        public FileSystemComponent(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Display(int space);
+        public abstract int GetSize();
+    }
+    class File : FileSystemComponent
+    {
+        int size;
+
+        public File(string name, int size) : base(name)
+        {
+            this.size = size;
+        }
+
+        public override void Display(int space)
+        {
+            Console.WriteLine(new string(' ', space) + Name + " (" + size + " KB)");
+        }
+
+        public override int GetSize()
+        {
+            return size;
+        }
+    }
+    class Directory : FileSystemComponent
+    {
+        List<FileSystemComponent> items = new List<FileSystemComponent>();
+
+        public Directory(string name) : base(name) { }
+
+        public void Add(FileSystemComponent c)
+        {
+            if (!items.Contains(c))
+                items.Add(c);
+            else
+                Console.WriteLine("Exist: " + c.Name);
+        }
+
+        public void Remove(FileSystemComponent c)
+        {
+            if (items.Contains(c))
+                items.Remove(c);
+            else
+                Console.WriteLine("Don't exist: " + c.Name);
+        }
+
+        public override void Display(int space)
+        {
+            Console.WriteLine(new string(' ', space) + "[" + Name + "]");
+
+            foreach (var i in items)
+                i.Display(space + 2);
+        }
+
+        public override int GetSize()
+        {
+            int sum = 0;
+            foreach (var i in items)
+                sum += i.GetSize();
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var root = new Directory("Root");
+            var folder = new Directory("Docs");
+
+            var f1 = new File("a.txt", 100);
+            var f2 = new File("b.txt", 200);
+
+            root.Add(f1);
+            root.Add(folder);
+            folder.Add(f2);
+
+            folder.Add(f2);     
+            root.Remove(f2);    
+
+            Console.WriteLine("Structure:");
+            root.Display(0);
+
+            Console.WriteLine("\n Root Size: " + root.GetSize() + " KB");
+        }
+    }
+}
